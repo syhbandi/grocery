6ea389195b5c9d478b1c5f34ce1997f3170ad209
syhbandi <saboonlbk@gmail.com>
2024-09-23 22:06:49 +0800

    init

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-white">
+      <Text className="font-[Poppins]">
+        Edit app/index.tsx to edit this screen. tes
+      </Text>
+    </View>
+  );
+}
